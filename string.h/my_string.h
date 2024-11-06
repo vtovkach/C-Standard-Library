68f5957ec943f7c1edbea08f7452a44af67637d6
Vadym <107150489+Vadim0036@users.noreply.github.com>
2024-11-05 22:03:04 -0600

    Add files via upload

--- string.h/my_string.h
+++ string.h/my_string.h
@@ -1,11 +1,13 @@
 #define MY_STRING_H
 #ifdef MY_STRING_H
 
+#include <stddef.h>
 
 size_t my_strlen(char str[]);
 
-int my_strcpy(char str1[], char str2[]);
+int my_strcpy(char *dest, const char *source);
 
+int _my_strcpy(char *dest, const char *source, size_t max_size);
 
 
 #endif
