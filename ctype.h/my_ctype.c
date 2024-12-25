a7231ae7e446b68c3306167ee8764b7f88baa165
Vadym <107150489+Vadim0036@users.noreply.github.com>
2024-12-25 01:27:02 -0600

    Add files via upload

--- ctype.h/my_ctype.c
+++ ctype.h/my_ctype.c
@@ -1,1 +1,12 @@
+#include "my_ctype.h"
 
+int __isalnum(const int c)
+{
+    if((c <= 0x39 && c>= 0x30)  ||  /* 0-9 */
+       (c >= 0x41 && c <= 0x5A) ||  /* A-Z */
+       (c >= 0x61 && c <= 0x7A))    /* a-z */
+    {
+        return 1;
+    }
+    return 0;
+}
