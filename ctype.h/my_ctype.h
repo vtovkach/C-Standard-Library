003d28306954967288985e261dd2bfd5aba34b4d
Vadym <107150489+Vadim0036@users.noreply.github.com>
2024-12-27 23:22:45 -0600

    Add files via upload

--- ctype.h/my_ctype.h
+++ ctype.h/my_ctype.h
@@ -3,4 +3,18 @@
 
 int __isalnum(const int c);
 
+
+int __isalpha(const int c);
+
+int __isblank(const int c);
+
+int __islower(const int c);
+
+int __isupper(const int c);
+
+
+int __tolower(int c);
+int __toupper(int c);
+
+
 #endif
