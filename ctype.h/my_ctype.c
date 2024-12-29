0feaaf464f5bae7c4d6c592e8cc49cfbe2a6f96b
Vadym <107150489+Vadim0036@users.noreply.github.com>
2024-12-28 22:23:45 -0600

    Add files via upload

--- ctype.h/my_ctype.c
+++ ctype.h/my_ctype.c
@@ -28,3 +28,45 @@ int __toupper(const int c)
     }
     return c; 
 }
+
+int __isalpha(const int c)
+{
+    if(c >= 0x41 && c <= 0x5A)
+    {
+        return 1;
+    }
+    else if(c >= 0x61 && c <= 0x7A)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int __isblank(const int c)
+{
+    if(c == 0x20 || c == 0x09)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int __islower(int c)
+{
+    int newChar = c & 0x20; 
+    if(newChar != 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int __isupper(int c)
+{
+    int newChar = c & 0x20;
+    if(newChar == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
