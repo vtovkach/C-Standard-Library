4021546d6faf6725e7ccb1b79f583fca8feeccc1
Vadym <107150489+Vadim0036@users.noreply.github.com>
2024-12-29 23:18:19 -0600

    Add files via upload

--- stdio.h/my_stdio.c
+++ stdio.h/my_stdio.c
@@ -1,1 +1,43 @@
+#include <unistd.h>
+#include <stdarg.h>
 
+#include "my_stdio.h"
+
+
+void my_printf(char const *string, ...)
+{
+    va_list arg;
+    int args = 0;
+    int i = 0;
+    char current_char; 
+    
+    current_char = string[i];
+    while(current_char != '\0')
+    {
+        if(current_char == '%')
+        {
+            args++;
+        }
+        i++;
+    }
+
+    
+    /*
+    i = 0;
+    current_char = string[i];
+    while(*string != '\0')
+    {
+        
+    }
+    */
+    
+}
+
+
+/*
+
+    %d integer
+    %f double 
+    ...
+
+*/
